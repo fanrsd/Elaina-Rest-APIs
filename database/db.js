26d849d63267dbf9c03afc54ab8d2aae8f1134f1
fanrsd <78183137+fanrsd@users.noreply.github.com>
2023-03-20 10:13:01 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.sorabot.xyz';
+var url = 'https://web-production-9bce8.up.railway.app';
 try {
-if(url == 'https://api.sorabot.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://web-production-9bce8.up.railway.app') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
